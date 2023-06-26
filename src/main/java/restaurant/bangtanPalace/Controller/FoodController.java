286d087e887e27535fa62c88907063de2c904b88
hanniel09 <hannielvieira1227@gmail.com>
2023-06-26 16:09:41 -0300

    Feature

--- src/main/java/restaurant/bangtanPalace/Controller/FoodController.java
+++ src/main/java/restaurant/bangtanPalace/Controller/FoodController.java
@@ -1,22 +1,34 @@
 package restaurant.bangtanPalace.Controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import restaurant.bangtanPalace.Service.FoodService;
 import restaurant.bangtanPalace.domain.Food;
+import restaurant.bangtanPalace.request.FoodPostRequestBody;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("cardapio")
 @RequiredArgsConstructor
 public class FoodController {
     private final FoodService foodService;
-    @GetMapping
+    @GetMapping()
     public ResponseEntity<Page<Food>> list(Pageable pageable){
         return ResponseEntity.ok(foodService.listAll(pageable));
+    }
+    @GetMapping(path = "/foods")
+    public ResponseEntity<List<Food>> listAll() {
+        return ResponseEntity.ok(foodService.listAllNonPageable());
+    }
+    @PostMapping
+    public ResponseEntity<Food> save(@RequestBody @Valid FoodPostRequestBody foodPostRequestBody){
+        return new ResponseEntity<>(foodService.save(foodPostRequestBody), HttpStatus.OK);
+
     }
 }
--- src/main/java/restaurant/bangtanPalace/Service/FoodService.java
+++ src/main/java/restaurant/bangtanPalace/Service/FoodService.java
@@ -1,11 +1,16 @@
 package restaurant.bangtanPalace.Service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import restaurant.bangtanPalace.Repository.FoodRepository;
 import restaurant.bangtanPalace.domain.Food;
+import restaurant.bangtanPalace.mapper.FoodMapper;
+import restaurant.bangtanPalace.request.FoodPostRequestBody;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +18,14 @@ public class FoodService {
     private final FoodRepository foodRepository;
     public Page<Food> listAll(Pageable pageable) {
         return foodRepository.findAll(pageable);
+    }
+
+    public List<Food> listAllNonPageable() {
+        return foodRepository.findAll();
+    }
+
+    @Transactional
+    public Food save(FoodPostRequestBody foodPostRequestBody){
+        return foodRepository.save(FoodMapper.INSTANCE.toFood(foodPostRequestBody));
     }
 }

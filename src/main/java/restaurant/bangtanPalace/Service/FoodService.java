d11f0103027f76b7da1c96b4bcc0536d9c304890
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-07-20 15:14:37 -0300

    feature

--- src/main/java/restaurant/bangtanPalace/Service/FoodService.java
+++ src/main/java/restaurant/bangtanPalace/Service/FoodService.java
@@ -2,6 +2,7 @@ package restaurant.bangtanPalace.Service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -11,13 +12,20 @@ import restaurant.bangtanPalace.exception.BadRequestException;
 import restaurant.bangtanPalace.mapper.FoodMapper;
 import restaurant.bangtanPalace.request.FoodPostRequestBody;
 import restaurant.bangtanPalace.request.FoodPutRequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FoodService {
     private final FoodRepository foodRepository;
+    private final S3Client s3Client;
+    @Value("${aws.s3.bucketName}")
+    private String bucketName;
+
     public Page<Food> listAll(Pageable pageable) {
         return foodRepository.findAll(pageable);
     }
@@ -32,7 +40,10 @@ public class FoodService {
     }
 
     @Transactional
-    public Food save(FoodPostRequestBody foodPostRequestBody){
+    public Food save(FoodPostRequestBody foodPostRequestBody) throws IOException {
+        // Convert Base64 image data to ByteBuffer
+        byte[] imageBytes = java.util.Base64.getDecoder().decode(foodPostRequestBody.getImage());
+        ByteBuffer imageByteBuffer = ByteBuffer.wrap(imageBytes);
         return foodRepository.save(FoodMapper.INSTANCE.toFood(foodPostRequestBody));
     }
 
